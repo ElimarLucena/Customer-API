960de44f6540916f98c67183480ff29a7b1993d5
Elimar Lucena <elimarlucenar@gmail.com>
2024-11-15 15:34:48 -0300

    test: create GetCustomerById_ReturnsOkResult_WithCustomer unit test.

--- tests/UnitTests/src/v1/Web.Api/Controllers/v1/CustomerController_Tests.cs
+++ tests/UnitTests/src/v1/Web.Api/Controllers/v1/CustomerController_Tests.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using UnitTests.util;
 using Web.Api.Controllers.v1;
 
 namespace UnitTests.src.v1.Web.Api.Controllers.v1
@@ -11,56 +12,24 @@ namespace UnitTests.src.v1.Web.Api.Controllers.v1
     public class CustomerController_Tests
     {
         private readonly Mock<IMediator> _mockMediator;
-        private static Guid _testId { get; set; }
 
-        private static List<GetAllCustomerResponse> _dataBaseMock
-        {
-            get
-            {
-                return
-                [
-                    new GetAllCustomerResponse()
-                    {
-                        CustomerId = _testId,
-                        Name = "User_1",
-                        Email = "User_1@gmail.com",
-                        Age = 20,
-                        Phone = 123456789,
-                        Document = "cpf"
-                    },
-                    new GetAllCustomerResponse()
-                    {
-                        CustomerId = _testId,
-                        Name = "User_2",
-                        Email = "User_2@gmail.com",
-                        Age = 21,
-                        Phone = 123456781,
-                        Document = "cpf1"
-                    },
-                ];
-            }
-        }
-
-        public CustomerController_Tests()
-        {
-            _mockMediator = new Mock<IMediator>();
-            Guid guid = Guid.NewGuid();
-            _testId = guid;
-        }
+        public CustomerController_Tests() => _mockMediator = new Mock<IMediator>();
 
         [Fact]
-        public async Task Get_ReturnsOkResult_WithAllCustomers()
+        public async Task GetAllCustomers_ReturnsOkResult_WithAllCustomers()
         {
             // Arrange
+            List<GetAllCustomersResponse> getAllCustomerResponseMock = DataBaseMock.GetAllCustomerResponseMock();
+
             CancellationToken cancellationToken = new();
 
             _mockMediator.Setup(moq => moq.Send(It.IsAny<GetAllCustomersRequest>(), It.IsAny<CancellationToken>()))
-                         .ReturnsAsync(_dataBaseMock);
+                         .ReturnsAsync(getAllCustomerResponseMock);
 
             CustomerController customerController = new(_mockMediator.Object);
 
             // Act
-            ActionResult<List<GetAllCustomerResponse>> getAllCustomers = await customerController.Get(cancellationToken);
+            ActionResult<List<GetAllCustomersResponse>> getAllCustomers = await customerController.GetAllCustomers(cancellationToken);
 
             // Assert
             OkObjectResult response = (OkObjectResult) getAllCustomers.Result!;
@@ -68,8 +37,8 @@ namespace UnitTests.src.v1.Web.Api.Controllers.v1
             response.Should().BeOfType(typeof(OkObjectResult));
             response.StatusCode.Should().Be(200);
 
-            List<GetAllCustomerResponse> responseValue = (List<GetAllCustomerResponse>) response.Value!;
-            responseValue[0].CustomerId.Should().Be(_dataBaseMock[0].CustomerId);
+            List<GetAllCustomersResponse> responseValue = (List<GetAllCustomersResponse>) response.Value!;
+            responseValue[0].CustomerId.Should().Be(getAllCustomerResponseMock[0].CustomerId);
             responseValue[0].Name.Should().Be("User_1");
             responseValue[0].Email.Should().Be("User_1@gmail.com");
             responseValue[0].Email.Should().Match("*@*.com");
@@ -77,13 +46,46 @@ namespace UnitTests.src.v1.Web.Api.Controllers.v1
             responseValue[0].Phone.Should().Be(123456789);
             responseValue[0].Document.Should().Be("cpf");
 
-            responseValue[1].CustomerId.Should().Be(_dataBaseMock[1].CustomerId);
+            responseValue[1].CustomerId.Should().Be(getAllCustomerResponseMock[1].CustomerId);
             responseValue[1].Name.Should().Be("User_2");
             responseValue[1].Email.Should().Be("User_2@gmail.com");
             responseValue[1].Email.Should().Match("*@*.com");
             responseValue[1].Age.Should().Be(21);
             responseValue[1].Phone.Should().Be(123456781);
             responseValue[1].Document.Should().Be("cpf1");
+        }
+
+
+        [Fact]
+        public async Task GetCustomerById_ReturnsOkResult_WithCustomer()
+        {
+            // Arrange
+            GetCustomerByIdResponse getCustomerByIdResponseMock = DataBaseMock.GetCustomerByIdResponseMock();
+
+            CancellationToken cancellationToken = new();
+
+            _mockMediator.Setup(moq => moq.Send(It.IsAny<GetCustomerByIdRequest>(), It.IsAny<CancellationToken>()))
+                         .ReturnsAsync(getCustomerByIdResponseMock);
+
+            CustomerController customerController = new(_mockMediator.Object);
+
+            // Act
+            ActionResult<GetCustomerByIdResponse> GetCustomerById = await customerController.GetCustomerById(getCustomerByIdResponseMock.CustomerId, cancellationToken);
+
+            // Assert
+            OkObjectResult response = (OkObjectResult) GetCustomerById.Result!;
+            response.Should().NotBeNull();
+            response.Should().BeOfType(typeof(OkObjectResult));
+            response.StatusCode.Should().Be(200);
+
+            GetCustomerByIdResponse responseValue = (GetCustomerByIdResponse) response.Value!;
+            responseValue.CustomerId.Should().Be(getCustomerByIdResponseMock.CustomerId);
+            responseValue.Name.Should().Be("User_1");
+            responseValue.Email.Should().Be("User_1@gmail.com");
+            responseValue.Email.Should().Match("*@*.com");
+            responseValue.Age.Should().Be(20);
+            responseValue.Phone.Should().Be(123456789);
+            responseValue.Document.Should().Be("cpf");
         }
     }
 }
