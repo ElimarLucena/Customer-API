0870e41d88d950c0f8f51e4209109a1a314dc24d
Elimar Lucena <elimarlucenar@gmail.com>
2024-11-15 15:31:55 -0300

    test: add more informations in tha data base mock.

--- tests/UnitTests/util/DataBaseMock.cs
+++ tests/UnitTests/util/DataBaseMock.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Application.Models.CustomerModels.Response;
+using Domain.Entities;
 
 namespace UnitTests.util
 {
@@ -32,6 +33,55 @@ namespace UnitTests.util
             }
         }
 
+        private static List<GetAllCustomersResponse> _getAllCustomerResponseMock
+        {
+            get
+            {
+                return
+                [
+                    new GetAllCustomersResponse()
+                    {
+                        CustomerId = Guid.NewGuid(),
+                        Name = "User_1",
+                        Email = "User_1@gmail.com",
+                        Age = 20,
+                        Phone = 123456789,
+                        Document = "cpf"
+                    },
+                    new GetAllCustomersResponse()
+                    {
+                        CustomerId = Guid.NewGuid(),
+                        Name = "User_2",
+                        Email = "User_2@gmail.com",
+                        Age = 21,
+                        Phone = 123456781,
+                        Document = "cpf1"
+                    },
+                ];
+            }
+        }
+
+        private static GetCustomerByIdResponse _getCustomerByIdResponseMock
+        {
+            get
+            {
+                return
+                    new GetCustomerByIdResponse()
+                    {
+                        CustomerId = Guid.NewGuid(),
+                        Name = "User_1",
+                        Email = "User_1@gmail.com",
+                        Age = 20,
+                        Phone = 123456789,
+                        Document = "cpf"
+                    };
+            }
+        }
+
         public static List<Customer> CustomerDBMock() => _customerDBMock;
+
+        public static List<GetAllCustomersResponse> GetAllCustomerResponseMock() => _getAllCustomerResponseMock;
+
+        public static GetCustomerByIdResponse GetCustomerByIdResponseMock() => _getCustomerByIdResponseMock;
     }
 }
