062ef5afb5371ce05ee40d03cadc07b564cca364
Elimar Lucena <elimarlucenar@gmail.com>
2024-11-20 18:32:23 -0300

    test: create GenerateToken_Returns_WithToken unit test.

---
+++ tests/UnitTests/src/v1/Application/Authentication/AuthenticationToken_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Application.Authentication;
+using Application.Interfaces;
+using Domain.Entities;
+using FluentAssertions;
+using Moq;
+using UnitTests.util;
+
+namespace UnitTests.src.v1.Application.Authentication
+{
+    public class AuthenticationToken_Tests
+    {
+        private readonly Mock<IAuthenticationToken> _authenticationToken;
+
+        public AuthenticationToken_Tests() => _authenticationToken = new Mock<IAuthenticationToken>();
+
+        [Fact]
+        public void GenerateToken_Returns_WithToken()
+        {
+            // Arrange
+            List<Customer> customerDBMock = DataBaseMock.CustomerDBMock();
+
+            string keyTest = Guid.NewGuid().ToString();
+
+            AuthenticationToken authenticationToken = new(keyTest);
+
+            // Act
+            string generateToken = authenticationToken.GenerateToken(customerDBMock[0]);
+
+            // Assert
+            generateToken.Should().NotBeNullOrEmpty();
+            generateToken.Should().BeOfType<string>();
+        }
+    }
+}
