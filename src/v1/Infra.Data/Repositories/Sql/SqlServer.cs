aa2d5d55ecc12f7eb8e6082fb149b5d92aab887b
Elimar Lucena <elimarlucenar@gmail.com>
2025-02-23 10:41:06 -0300

    refactor: changing database name.

--- src/v1/Infra.Data/Repositories/Sql/SqlServer.cs
+++ src/v1/Infra.Data/Repositories/Sql/SqlServer.cs
@@ -3,24 +3,24 @@
     public static class SqlServer
     {
         public static string GetAllCustomers_Query() => 
-            "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM [Customer].[dbo].[TB_CUSTOMERS];";
+            "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM [CUSTOMER].[dbo].[TB_CUSTOMERS];";
 
         public static string GetCustomerById_Query() =>
-            "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM  [Customer].[dbo].[TB_CUSTOMERS] WHERE CustomerId = @customerId;";
+            "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM  [CUSTOMER].[dbo].[TB_CUSTOMERS] WHERE CustomerId = @customerId;";
 
         public static string GetCustomerByDocument_Query() =>
-            "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM [Customer].[dbo].[TB_CUSTOMERS] WHERE Document = @document;";
+            "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM [CUSTOMER].[dbo].[TB_CUSTOMERS] WHERE Document = @document;";
 
         public static string CreateCustomer_Command() =>
-            "INSERT INTO [Customer].[dbo].[TB_CUSTOMERS](CustomerId, Name, Email, Document, Phone, Age, Password) VALUES (@customerId, @name, @email, @document, @phone, @age, @password);";
+            "INSERT INTO [CUSTOMER].[dbo].[TB_CUSTOMERS](CustomerId, Name, Email, Document, Phone, Age, Password) VALUES (@customerId, @name, @email, @document, @phone, @age, @password);";
 
         public static string UpdateCustomer_Command() =>
-            "UPDATE [Customer].[dbo].[TB_CUSTOMERS] SET Name = @name, Email = @email, Document = @document, Phone = @phone, Age =  @age, Password = @password WHERE CustomerId = @customerId;";
+            "UPDATE [CUSTOMER].[dbo].[TB_CUSTOMERS] SET Name = @name, Email = @email, Document = @document, Phone = @phone, Age =  @age, Password = @password WHERE CustomerId = @customerId;";
 
         public static string DeleteCustomer_Command() =>
-            "DELETE FROM [Customer].[dbo].[TB_CUSTOMERS] WHERE CustomerId = @CustomerId;";
+            "DELETE FROM [CUSTOMER].[dbo].[TB_CUSTOMERS] WHERE CustomerId = @CustomerId;";
 
         public static string GetCustomerByEmailPassword_Query() =>
-           "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM [Customer].[dbo].[TB_CUSTOMERS] WHERE Email = @email AND Password = @password;";
+           "SELECT [CustomerId], [Name], [Email], [Age], [Phone], [Document] FROM [CUSTOMER].[dbo].[TB_CUSTOMERS] WHERE Email = @email AND Password = @password;";
     }
 }
