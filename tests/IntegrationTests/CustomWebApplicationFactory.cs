6e9a0c24a762ba81dde5139ee5871342ce4f42f1
Elimar Lucena <elimarlucenar@gmail.com>
2025-03-07 21:13:14 -0300

    refactor:  refactoring CustomWebApplicationFactory class.

--- tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -4,30 +4,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using System.Data;
-using Testcontainers.MsSql;
 
 namespace IntegrationTests
 {
     public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram 
                                                        : class
     {
-        private MsSqlContainer _container;
-
-        public CustomWebApplicationFactory()
-        {
-            const ushort HttpPort = 1433;
-
-            _container = new MsSqlBuilder()
-                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-                .WithEnvironment("ACCEPT_EULA", "Y")
-                .WithPortBinding(HttpPort, true)
-                .Build();
-
-            _container.StartAsync().Wait();
-
-            CreateTestDataBase.CreateDataBase(_container.GetConnectionString());
-        }
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
@@ -35,13 +17,8 @@ namespace IntegrationTests
                 var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IDbConnection));
                 services.Remove(dbContextDescriptor!);
 
-                services.AddScoped<ISqlServerDataBaseContext>(db => new SqlServerDataBaseContext(_container.GetConnectionString()));
+                services.AddScoped<ISqlServerDataBaseContext>(db => new SqlServerDataBaseContext(CreateTestDataBase.GetConnectionString()));
             });
-        }
-
-        public void StopContainerAsync()
-        {
-             _container.StopAsync().Wait();
         }
     }
 }
