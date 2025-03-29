49836fae74684b8ae3405f43cf8b78a4974ec66c
Elimar Lucena <elimarlucenar@gmail.com>
2025-03-29 12:27:41 -0300

    test: adding override authentication configs to the integration test.

--- tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,9 +1,12 @@
 ﻿using Infra.Data.DbContext;
 using IntegrationTests.Util;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Data;
+using System.Text;
 
 namespace IntegrationTests
 {
@@ -23,6 +26,23 @@ namespace IntegrationTests
                 services.Remove(dbContextDescriptor!);
 
                 services.AddScoped<ISqlServerDataBaseContext>(db => new SqlServerDataBaseContext(_connectionString));
+
+                // override authentication configs.
+                services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = "http://localhost:63651",
+                        ValidAudience = "http://localhost:4200",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+                        (
+                            "JWTAuthenticationSecuredTest7630b55d7c954cf293283686889427ec"
+                        )),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateIssuerSigningKey = true
+                    };
+                });
             });
         }
     }
