8bd659facdfe53df3e6fcbf84c57ed13686f7eda
Elimar Lucena <elimarlucenar@gmail.com>
2025-04-19 15:31:44 -0300

    test: adding integration tests.

--- tests/IntegrationTests/Util/CreateTestDataBase.cs
+++ tests/IntegrationTests/Util/CreateTestDataBase.cs
@@ -47,7 +47,9 @@ namespace IntegrationTests.Util
                     (CustomerId, Name, Email, Document, Phone, Age, Password)
                 VALUES 
                     ('{Guid.Parse("3b848ecb-8611-409c-b741-634f8f053ba6")}', 'TestCustomer', 'testcustomer@gmail.com', 'cpf', 123456789, 26, 'testcustomer123'),
-                    ('{Guid.Parse("b5125fed-3c62-4809-af18-8e201beaf4ec")}', 'TestCustomer2', 'testcustomer2@gmail.com', 'cpf2',  123493789, 37, 'testcustomer2123');",
+                    ('{Guid.Parse("b5125fed-3c62-4809-af18-8e201beaf4ec")}', 'TestCustomer2', 'testcustomer2@gmail.com', 'cpf2', 123493789, 37, 'testcustomer2123'),
+                    ('{Guid.Parse("79af865d-9f4b-4777-a52e-a0f46b086ddd")}', 'TestCustomer4', 'testcustomer4@gmail.com', 'cpf4', 123456789, 22, 'testcustomer4123'),
+                    ('{Guid.Parse("a3fe6701-2c8a-4c7c-8309-07050fccdee7")}', 'TestCustomer5', 'testcustomer5@gmail.com', 'cpf5', 123493789, 18, 'testcustomer5123');",
             ];
 
             SqlConnection connection = new(connectionString);
