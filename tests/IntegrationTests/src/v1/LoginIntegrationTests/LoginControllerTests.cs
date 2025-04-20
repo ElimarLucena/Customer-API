e622f14ef76b2eb556131c74e011b8f9ecff2ba3
Elimar Lucena <elimarlucenar@gmail.com>
2025-04-20 10:36:09 -0300

    test: create LoginToken_Returns_JWTToken intergration test.

--- tests/IntegrationTests/src/v1/LoginIntegrationTests/LoginControllerTests.cs
+++ tests/IntegrationTests/src/v1/LoginIntegrationTests/LoginControllerTests.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using Application.Models.LoginModels.Request;
+using Application.Models.LoginModels.Response;
+using FluentAssertions;
+using IntegrationTests.Util;
+using Newtonsoft.Json;
+using Xunit;
 
 namespace IntegrationTests.src.v1.LoginIntegrationTests
 {
-    internal class LoginControllerTests
+    public class LoginControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>, IDisposable
     {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public LoginControllerTests(CustomWebApplicationFactory<Program> factory)
+            => _factory = factory;
+
+        [Fact]
+        public async Task LoginToken_Returns_JWTToken()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+
+            LoginCustomerRequest loginCustomerRequest = new()
+            {
+                Email = "testcustomer@gmail.com",
+                Password = "testcustomer123"
+            };
+
+            StringContent content = new(
+                JsonConvert.SerializeObject(loginCustomerRequest),
+                Encoding.UTF8, 
+                "application/json"
+            );
+
+            const string URI = $"/api/v1/login/getToken";
+
+            // Act
+            HttpResponseMessage response = await client.PostAsync(URI, content);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            LoginCustomerResponse loginCustomerResponse = JsonConvert.DeserializeObject<LoginCustomerResponse>(responseContent)!;
+            loginCustomerResponse.Token.Should().NotBeNullOrEmpty()
+                .And.BeOfType(typeof(string));
+        }
+
+        public void Dispose()
+        {
+            CreateTestDataBase.StopContainerAsync();
+        }
     }
 }
